b641be7e8e175ac780a47dc3a005653755e71ed8
divine higgins <divinehiggins@divines-MacBook-Pro.local>
2021-07-28 10:59:55 -0400

    inital setup in server.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 // old syntax
 // const express = require("express");
+// type: module
 import express from "express";
+import cors from "cors"
+import morgan from "morgan";
+
+const app = express()
+const PORT = process.env.PORT || 4567;
+
+// gives acess to req.body
+app.use(express.json())
+app.use(cors());
+// change after deploy and we know everything is working"tiny
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("<h1>hello,world</h1>"))
+
+app.listen(PORT, console.log(`connected to port: ${PORT}`))
