c5374422c8414e367adb8357ba58544d825b6bca
divine higgins <divinehiggins@divines-MacBook-Pro.local>
2021-07-28 15:16:43 -0400

    added helpers

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import usersRoutes from "./user.js"
+import todoRoutes from "./todo.js"
 
 const router = Router();
 
 router.get("/", (req, res) => res.send("this is api root"));
 
-router.use("/", usersRoutes)
-// router.use("/", todoRoutes)
+router.use("/", usersRoutes);
+router.use("/", todoRoutes);
 
 export default router;
---
+++ routes/todo.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import restrict from "../helpers/restrict";
+const router = Router()
+// get  request all todos
+router.use("/todos", getAllTodos);
+// get  request id todo
+router.use("/todos/:id", getTodo);
+// post request  createtodo
+router.post("/todos/:id", restrict, createTodo);
+// put request updatetodo
+router.put("/todos/:id", restrict, updateTodo);
+// delete todo
+router.delete("/todos/:id", restrict, deleteTodo);
+export default router;
